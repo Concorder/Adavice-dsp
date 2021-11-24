229233fc04a243b1af3464512822edd2c28d3fd0
MarinaKomyna <63154313+MarinaKomyna@users.noreply.github.com>
2021-11-24 14:26:36 +0200

    updated blog files

---
+++ js/blog_script.js
@@ -0,0 +1,15 @@
+$('.share_icon_1').mouseenter(function () {
+    $('.share_icon_1 path').css('fill', '#10345A');
+});
+
+$('.share_icon_1').mouseleave(function () {
+    $('.share_icon_1 path').css('fill', '#8C8C8C');
+});
+
+$('.share_icon_2').mouseenter(function () {
+    $('.share_icon_2 path').css('fill', '#10345A');
+});
+
+$('.share_icon_2').mouseleave(function () {
+    $('.share_icon_2 path').css('fill', '#8C8C8C');
+});
