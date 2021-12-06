7bf17cd7ef2980f023ba47364390a2ba2caaa607
MarinaKomyna <63154313+MarinaKomyna@users.noreply.github.com>
2021-12-06 16:35:40 +0200

    updated blog files

--- js/blog_script.js
+++ js/blog_script.js
@@ -1,3 +1,19 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    if (window.innerWidth > 980) {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".blog_header").style.top = "0";
+        } else {
+            document.querySelector(".blog_header").style.top = "-95px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+    else{
+        document.querySelector(".blog_header").style.top = "0";
+    }
+}
+
 $('.share_icon_1').mouseenter(function () {
     $('.share_icon_1 path').css('fill', '#10345A');
 });
