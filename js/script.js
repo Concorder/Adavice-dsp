662d895f099c548845aa3282c63eb009ee5ca639
Concorder <arenko.design@gmail.com>
2022-01-11 17:14:43 +0200

    refactor

--- js/script.js
+++ js/script.js
@@ -22,5 +22,62 @@ $(document).ready(function () {
 
 let today = new Date();
 let year = today.getFullYear();
-$('#year').text(year);
+document.querySelector('#year').innerText = year;
+
+//filters and tags
+//if there are tags on page
+const tags = document.querySelectorAll('.tag');
+if (tags.length > 0) {
+    const articleList = document.querySelectorAll(".article_container");
+    let filterBase = {};
+    let filterRequest = []
+    tags.forEach(tag => {
+        tag.addEventListener("click", filterByTag)
+        if (filterBase[tag.innerHTML]) {
+            filterBase[tag.innerHTML]++;
+        } else {
+            filterBase[tag.innerHTML] = 1
+        }
+    });
+    console.log(filterBase)
+    
+    if (window.location.href.indexOf("search") > -1 || sessionStorage.getItem('filterRequest').length > 0) {
+        filterRequest = [sessionStorage.getItem('filterRequest')];
+        renderResults()
+    }
+    
+    function filterByTag() {
+        if (window.location.href.indexOf("search") > -1) {
+            sessionStorage.setItem('filterRequest', '')
+            filterRequest = []
+            this.classList.toggle("active");
+            document.querySelectorAll(".tag.active").forEach(tag => {
+                filterRequest.push(tag.innerHTML);
+            });
+            console.log(filterRequest)
+            renderResults();
+            
+            
+        } else {
+            sessionStorage.setItem("filterRequest", this.innerHTML);
+            window.location.pathname = 'Adavice-dsp/search.html';
+        }
+    }
+    
+    function renderResults() {
+        articleList.forEach(article => {
+            article.setAttribute("data-filter", "0");
+            article.querySelectorAll('.tag').forEach(tag => {
+                if (filterRequest.indexOf(tag.innerHTML) !== -1 || article.getAttribute("data-filter") === "1") {
+                    article.setAttribute("data-filter", "1");
+                } else {
+                    tag.setAttribute("data-filter", "0")
+                }
+            });
+            if (article.getAttribute("data-filter") !== "1") {
+                article.style.display = "none"
+            }
+        })
+    }
+}
 
