fd3e32ea8d4a07c1daf8adb9b9f00303abb0a995
Concorder <arenko.design@gmail.com>
2022-01-11 19:42:16 +0200

    filters

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-    /*$(document).on('click', 'a[href^="#"]', function (event) {
-        event.preventDefault();
+	/*$(document).on('click', 'a[href^="#"]', function (event) {
+		event.preventDefault();
 
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
-    });*/
-
-    $('#services').hover(function () {
-        $('#services_sub').slideDown(200);
-    }, function () {
-        $('#services_sub').slideUp(50);
-    });
-
-    $('#company').hover(function () {
-        $('#company_sub').slideDown(200);
-    }, function () {
-        $('#company_sub').slideUp(50);
-    });
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 500);
+	});*/
+	
+	$('#services').hover(function () {
+		$('#services_sub').slideDown(200);
+	}, function () {
+		$('#services_sub').slideUp(50);
+	});
+	
+	$('#company').hover(function () {
+		$('#company_sub').slideDown(200);
+	}, function () {
+		$('#company_sub').slideUp(50);
+	});
 });
 
 let today = new Date();
@@ -28,56 +28,67 @@ document.querySelector('#year').innerText = year;
 //if there are tags on page
 const tags = document.querySelectorAll('.tag');
 if (tags.length > 0) {
-    const articleList = document.querySelectorAll(".article_container");
-    let filterBase = {};
-    let filterRequest = []
-    tags.forEach(tag => {
-        tag.addEventListener("click", filterByTag)
-        if (filterBase[tag.innerHTML]) {
-            filterBase[tag.innerHTML]++;
-        } else {
-            filterBase[tag.innerHTML] = 1
-        }
-    });
-    console.log(filterBase)
-    
-    if (window.location.href.indexOf("search") > -1 || sessionStorage.getItem('filterRequest').length > 0) {
-        filterRequest = [sessionStorage.getItem('filterRequest')];
-        renderResults()
-    }
-    
-    function filterByTag() {
-        if (window.location.href.indexOf("search") > -1) {
-            sessionStorage.setItem('filterRequest', '')
-            filterRequest = []
-            this.classList.toggle("active");
-            document.querySelectorAll(".tag.active").forEach(tag => {
-                filterRequest.push(tag.innerHTML);
-            });
-            console.log(filterRequest)
-            renderResults();
-            
-            
-        } else {
-            sessionStorage.setItem("filterRequest", this.innerHTML);
-            window.location.pathname = 'Adavice-dsp/search.html';
-        }
-    }
-    
-    function renderResults() {
-        articleList.forEach(article => {
-            article.setAttribute("data-filter", "0");
-            article.querySelectorAll('.tag').forEach(tag => {
-                if (filterRequest.indexOf(tag.innerHTML) !== -1 || article.getAttribute("data-filter") === "1") {
-                    article.setAttribute("data-filter", "1");
-                } else {
-                    tag.setAttribute("data-filter", "0")
-                }
-            });
-            if (article.getAttribute("data-filter") !== "1") {
-                article.style.display = "none"
-            }
-        })
-    }
+	const articleList = document.querySelectorAll(".article_container");
+	let filterBase = [];
+	let filterRequest = [];
+	if (window.location.href.indexOf("search") > -1 && sessionStorage.getItem('filterRequest')) {
+		filterRequest = [sessionStorage.getItem('filterRequest')];
+		renderResults();
+	}
+	
+	tags.forEach(tag => {
+		tag.addEventListener("click", filterByTag)
+		if (filterBase.indexOf(tag.innerHTML) < 0) {
+			filterBase.push(tag.innerHTML)
+		}
+	});
+	
+	console.log(filterBase)
+	tagPool = document.querySelector(".tag_container.pool")
+	filterBase.forEach(el => {
+		addTag(el)
+	})
+	
+	function addTag(el) {
+		let newTag = document.createElement("a");
+		newTag.innerText = el;
+		newTag.classList.add("tag");
+		tagPool.appendChild(newTag);
+	}
+	
+	function filterByTag() {
+		if (window.location.href.indexOf("search") > -1) {
+			sessionStorage.setItem('filterRequest', '')
+			filterRequest = []
+			this.classList.toggle("active");
+			document.querySelectorAll(".tag.active").forEach(tag => {
+				if (filterRequest.indexOf(tag.innerHTML) < 0) {
+					filterRequest.push(tag.innerHTML);
+				}
+				
+			});
+			console.log(filterRequest)
+			renderResults();
+		} else {
+			sessionStorage.setItem("filterRequest", this.innerHTML);
+			window.location.pathname = 'Adavice-dsp/search.html';
+		}
+	}
+	
+	function renderResults() {
+		document.querySelector(".first_block h2").innerText = filterRequest
+		articleList.forEach(article => {
+			article.setAttribute("data-filter", "0");
+			article.querySelectorAll('.tag').forEach(tag => {
+				if (filterRequest.indexOf(tag.innerHTML) !== -1 || article.getAttribute("data-filter") === "1") {
+					article.setAttribute("data-filter", "1");
+					article.classList.remove("hidden")
+				} else {
+					tag.setAttribute("data-filter", "0")
+					article.classList.add("hidden")
+				}
+			});
+		})
+	}
 }
 
